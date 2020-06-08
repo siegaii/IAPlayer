04d8b8f972caf088a2fc71a47ca0e7f6b4bf1cf2
siegaii <siiegaiii@gmail.com>
2020-06-08 17:09:33 +0800

    base video

--- src/ts/interactive-player.ts
+++ src/ts/interactive-player.ts
@@ -1,11 +1,20 @@
 class InteractivePlayer {
-    playerDom: Element;
-    h5Player: any;
+    public playerDom: Element;
+    public h5Player: HTMLVideoElement;
     constructor() {
         this.generatePlayer();
     }
+
+    /**
+     * 设置播放器style
+     * @param player h5播放器dom
+     */
+    public setPlayerStyle(player: HTMLVideoElement) {
+        player.setAttribute('style', `width:100%;height:100%;`);
+    }
+
     private generatePlayer(): void {
-        // 获取player容器dom
+        // 获取player容器
         this.playerDom = document.getElementsByTagName('interactive-player')[0];
         // 创建h5player节点
         this.h5Player = document.createElement('video');
@@ -13,25 +22,10 @@ class InteractivePlayer {
         this.setPlayerStyle(this.h5Player);
 
         // 设置播放源
-        this.h5Player.setAttribute('controls', true);
-        this.h5Player.setAttribute('src', '../static/video/test.mp4');
+        this.h5Player.setAttribute('controls', '');
+        this.h5Player.setAttribute('src', '../static/video/test2.mp4');
 
         // 将播放器插入interactive-player节点
         this.playerDom.appendChild(this.h5Player);
-    }
-
-    public play() {
-        this.h5Player.play();
-    }
-
-    /**
-     * 设置播放器style
-     * @param player h5播放器dom
-     */
-    public setPlayerStyle(player: any) {
-        player.setAttribute(
-            'style',
-            `width:100%;height:100%;background-color: #cccccc;`
-        );
     }
 }
---
+++ src/ts/main.ts
@@ -0,0 +1,5 @@
+// 测试
+(function () {
+    // 获取播放器对象
+    const Player = new InteractivePlayer();
+})();
