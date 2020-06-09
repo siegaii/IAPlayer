63136d36d4d2ff96f6bc7b37f41b80b86283584a
siegaii <siiegaiii@gmail.com>
2020-06-09 15:22:26 +0800

    .

--- public/js/interactive-player.js
+++ public/js/interactive-player.js
@@ -3,7 +3,7 @@ var InteractivePlayer = (function () {
         this.generatePlayer();
     }
     InteractivePlayer.prototype.setPlayerStyle = function (player) {
-        player.setAttribute('style', "width:100%;height:100%;");
+        player.setAttribute('style', 'width:100%;height:100%;');
     };
     InteractivePlayer.prototype.generatePlayer = function () {
         this.playerDom = document.getElementsByTagName('interactive-player')[0];
@@ -20,23 +20,31 @@ var InteractivePlayer = (function () {
     };
     InteractivePlayer.prototype.addBtnLay = function () {
         var _this = this;
-        this.h5Player.addEventListener('timeupdate', function () {
-            var time = Math.floor(_this.h5Player.currentTime);
-            if (time === 2) {
-                var btn1 = document.createElement('button');
-                btn1.textContent = 'test1';
-                btn1.className = 'test1';
-                var btn2 = document.createElement('button');
-                btn2.textContent = '全屏';
-                btn2.className = 'test2';
-                btn2.addEventListener('click', function () {
-                    console.log('全屏');
-                    _this.playerDom.webkitEnterFullscreen();
-                }, false);
-                _this.playerDom.appendChild(btn1);
-                _this.playerDom.appendChild(btn2);
-            }
-        }, false);
+        this.h5Player.addEventListener(
+            'timeupdate',
+            function () {
+                var time = Math.floor(_this.h5Player.currentTime);
+                if (time === 2) {
+                    var btn1 = document.createElement('button');
+                    btn1.textContent = 'test1';
+                    btn1.className = 'test1';
+                    var btn2 = document.createElement('button');
+                    btn2.textContent = '全屏';
+                    btn2.className = 'test2';
+                    btn2.addEventListener(
+                        'click',
+                        function () {
+                            console.log('全屏');
+                            _this.playerDom.webkitEnterFullscreen();
+                        },
+                        false
+                    );
+                    _this.playerDom.appendChild(btn1);
+                    _this.playerDom.appendChild(btn2);
+                }
+            },
+            false
+        );
     };
     return InteractivePlayer;
-}());
+})();
