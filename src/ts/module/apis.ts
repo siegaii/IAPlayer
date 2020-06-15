47381f4c383b35064c5e58564cf6e7677ac5e13f
“siegaii” <siiegaiii@gmail.com>
2020-06-15 11:23:21 +0800

    feat:0.0.0

---
+++ src/ts/module/apis.ts
@@ -0,0 +1,9 @@
+/**
+ * 为播放器对象挂载api
+ * @param player
+ */
+export const loadAPIForPlayer = (player: Element): void => {
+    console.log(player);
+};
+
+// const setPlayerSource = (src: String) => {};
