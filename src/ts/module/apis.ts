c0840f6884f044b215e02fb63ba4515b69995810
siegaii <siiegaiii@gmail.com>
2020-07-20 13:54:01 +0800

    init:reset project player1.0.0

--- src/ts/module/apis.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 为播放器对象挂载api
- * @param player
- */
-export const loadAPIForPlayer = (player: Element): void => {
-    console.log(player);
-};
-
-// const setPlayerSource = (src: String) => {};
